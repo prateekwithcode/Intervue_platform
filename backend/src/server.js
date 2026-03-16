229542a7780216268353a0196dae94f2e66c5f56
prateekwithcode <parteekkhatri2004@gmail.com>
2026-03-16 18:01:26 +0530

    change backend deployment code

--- backend/src/server.js
+++ backend/src/server.js
@@ -31,10 +31,12 @@ app.get("/health", (req, res) => {
 
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  const frontendPath = path.join(__dirname, "../frontend/dist");
 
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  app.use(express.static(frontendPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
 
