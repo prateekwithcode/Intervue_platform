8c4505c2adb7bb3a16396d41a4dc0a2343f47b72
prateekwithcode <parteekkhatri2004@gmail.com>
2026-03-16 17:51:31 +0530

    set path

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,7 +34,7 @@ if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
